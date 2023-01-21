fb3862dd17d0aac05af45c35a83131b1a8c86ff9
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2023-01-20 19:41:50 -0700

    prepare prod env folder

--- makeEnv.sh
+++ makeEnv.sh
@@ -1,6 +1,43 @@
 #!/usr/bin/env sh
-_env="dev"
-_envFileName=glitchenv
+# _env="dev"
+while :;do
+  case "$1" in
+    -h|--help)
+      show_help
+      shift 2
+      ;;
+    -e)
+      _env=$2
+      shift 2
+      ;;
+    -?*)
+      printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+      show_help
+      shift 2
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+show_help()
+{
+  # Display show_help
+  echo "Generates env file"
+  echo
+  echo "Syntax: ./makeEnv.sh -e <dev>"
+  echo "options:"
+  echo "  -e            REQUIRED - environment/folder to use."
+  echo "  -h|--help     Print this show_help."
+  echo
+  exit 0
+}
+
+if test -z "${_env}" ; then
+  show_help
+fi
+_envFileName="glitchenv${_env}"
 printf "" > "$_envFileName"
 for i in BXI_API_URL BXI_SDK_TOKEN_URL BXI_API_KEY BXI_COMPANY_ID ; do
   _lower=$(echo "$i" | tr '[:upper:]' '[:lower:]')
--- prod/vars.tf
+++ prod/vars.tf
@@ -9,5 +9,5 @@ variable "pingone_license_id" {
 
 variable "environment_name" {
   description = "name that will be used when creating PingOne Environment"
-  default = "sko-cicd-dev"
+  default = "sko-cicd-prod"
 }
